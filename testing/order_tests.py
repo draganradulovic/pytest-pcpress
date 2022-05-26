539c71d67f94bf3bf976dc511c1bb5e452eac6f3
draganradulovic <dragan.r3@gmail.com>
2022-05-26 16:46:23 +0200

    como

--- testing/order_tests.py
+++ testing/order_tests.py
@@ -16,35 +16,38 @@ class Order_tests(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def setingUp(self, oneTimeSetUp6, setUp6):
-        self.pp=Prodavnica_page(self.driver)
-        self.np=Prodavnica_naruceno_page(self.driver)
+        self.pp = Prodavnica_page(self.driver)
+        self.np = Prodavnica_naruceno_page(self.driver)
         self.log = cl.customLogger(logging.DEBUG)
 
     @pytest.mark.run(order=1)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_positive_order_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_positive_order_data.csv'))))
     @unpack
     def test_positive_order(self,magazines, quantity, name, adress, zip, city, phone, email):
-        ordering_magasines=magazines.split(",")
-        ordering_values=quantity.split(",")
+        ordering_magasines = magazines.split(",")
+        ordering_values = quantity.split(",")
         self.pp.order(ordering_magasines, ordering_values, name, adress, zip, city, phone, email)
-        result=self.np.is_ordered_successfuly()
+        result = self.np.is_ordered_successfuly()
         assert result == True
 
 
     @pytest.mark.run(order=2)
     def test_empty_order(self):
-        result=self.pp.empty_order()
+        result = self.pp.empty_order()
         assert result == True
 
     @pytest.mark.run(order=3)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_invalid_quantity_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_invalid_quantity_data.csv'))))
     @unpack
     def test_invalid_quantity(self,magazine, quantity):
-        result=self.pp.invalid_quantity_order(magazine,quantity)
+        result = self.pp.invalid_quantity_order(magazine,quantity)
         assert result == True
 
     @pytest.mark.run(order=4)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_invalid_drpd_select_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_invalid_drpd_select_data.csv'))))
     @unpack
     def test_invalid_drpd_select(self, magazine, item):
         items=item.split(",")
@@ -52,17 +55,19 @@ class Order_tests(unittest.TestCase):
         assert result == True
 
     @pytest.mark.run(order=5)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_invalid_name_field_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_invalid_name_field_data.csv'))))
     @unpack
     def test_invalid_name_field(self,magazines, quantity, name, address, zip, city, phone, email):
         ordering_magasines = magazines.split(",")
         ordering_values = quantity.split(",")
         self.pp.order(ordering_magasines, ordering_values, name, address, zip, city, phone, email)
-        result=self.np.ime_i_prezime_error_present()
+        result = self.np.ime_i_prezime_error_present()
         assert result == True
 
     @pytest.mark.run(order=6)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_invalid_address_field_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_invalid_address_field_data.csv'))))
     @unpack
     def test_invalid_address_field(self, magazines, quantity, name, address, zip, city, phone, email):
         ordering_magasines = magazines.split(",")
@@ -72,7 +77,8 @@ class Order_tests(unittest.TestCase):
         assert result == True
 
     @pytest.mark.run(order=7)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_invalid_phone_field_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_invalid_phone_field_data.csv'))))
     @unpack
     def test_invalid_phone_field(self, magazines, quantity, name, address, zip, city, phone, email):
         ordering_magasines = magazines.split(",")
@@ -82,7 +88,8 @@ class Order_tests(unittest.TestCase):
         assert result == True
 
     @pytest.mark.run(order=8)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_invalid_email_field_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_invalid_email_field_data.csv'))))
     @unpack
     def test_invalid_email_field(self, magazines, quantity, name, address, zip, city, phone, email):
         ordering_magasines = magazines.split(",")
@@ -92,7 +99,8 @@ class Order_tests(unittest.TestCase):
         assert result == True
 
     @pytest.mark.run(order=9)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_empty_ordering_form_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'test_empty_ordering_form_data.csv'))))
     @unpack
     def test_empty_ordering_form(self, magazines, quantity, name, address, zip, city, phone, email):
         ordering_magasines = magazines.split(",")
@@ -102,7 +110,8 @@ class Order_tests(unittest.TestCase):
         assert result == True
 
     @pytest.mark.run(order=10)
-    @data(*get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\verify_zip_city_sync_data.csv"))
+    @data(*get_csv_data(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'utilities','csv_data_files',
+                                                      'verify_zip_city_sync_data.csv'))))
     @unpack
     def test_city_zip_sync(self, magazines, quantity, name, address, zip, city, phone, email):
         result = self.pp.verify_zip_city_sync(magazines, quantity, name, address, zip, city, phone, email)
--- utilities/read_data.py
+++ utilities/read_data.py
@@ -6,10 +6,7 @@ def get_csv_data(fileName):
     reader=csv.reader(dataFile, delimiter=";")
     print(reader)
     next(reader)
-    print('**********************')
     for row in reader:
-        print(row)
         rows.append(row)
     return rows
 
-#get_csv_data(r"C:\Users\User\PycharmProjects\PCpress\utilities\test_data.csv")
